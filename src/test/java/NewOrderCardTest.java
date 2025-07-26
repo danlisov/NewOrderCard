5f8b5f33b16aa46833602ff36feb177eb2d25149
Dan <dan.lysov2015@yandex.ru>
2025-07-26 12:47:43 +0300

    Change NewOrderCardTest

--- src/test/java/NewOrderCardTest.java
+++ src/test/java/NewOrderCardTest.java
@@ -1,2 +1,36 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class NewOrderCardTest {
+    private WebDriver driver;
+
+    @BeforeAll
+    static void setUpAll() {
+        System.setProperty("webdriver.chrome.driver", "/path/to/chromedriver");
+        WebDriverManager.chromediver().setup();
+    }
+
+    @BeforeEach
+    void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+        driver.get("http://localhost:9999/");
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    void test() {
+
+    }
+
 }
